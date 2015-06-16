2050e78ce8c00639c53527f13f1ea6b40af47d78
htyuan <htyuan80@gmail.com>
2015-06-16 17:27:30 +0800

    加入pulltoRefresh

--- app/src/main/java/com/seven/pulltorefreshexplore/MainActivity.java
+++ app/src/main/java/com/seven/pulltorefreshexplore/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
 
 public class MainActivity extends ActionBarActivity {
 
